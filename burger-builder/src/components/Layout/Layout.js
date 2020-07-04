2568968b508f1d1a95ab0f12ead5e7e15e387535
omrpravin@gmail.com <omrpravin@gmail.com>
2020-07-04 21:03:56 +0530

    added menu related things

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar'
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) =>{
-    return(
-        <Aux>
-            <Toolbar></Toolbar>
-            <main className={classes.Content}>{props.children}
-            </main>
-        </Aux>);
+class Layout extends Component{
+
+    state ={
+        showSideDrawer:true
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState({
+            showSideDrawer:false
+        })
+    }
+
+    sideDrawerToggleHandler =() => {
+        this.setState((prevState) => {
+            return(
+                {showSideDrawer : !prevState.showSideDrawer}
+                );
+        })
+    }
+    render(){
+
+        return(
+            <Aux>
+                <Toolbar sideDrawerToggleHandler={this.sideDrawerToggleHandler}></Toolbar>
+                <SideDrawer open={this.state.showSideDrawer} close={this.sideDrawerClosedHandler}/>
+                <main className={classes.Content}>{this.props.children}
+                </main>
+            </Aux>
+        );
+    }
 }
-
-export default layout;
+export default Layout;
--- burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,12 +2,15 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle'
 const toolbar = (props) =>{
     return (
         <header className={classes.Toolbar}>
-            <div>MENU</div>
-            <Logo></Logo>
-            <nav>
+            <DrawerToggle sideDrawerToggleHandler={props.sideDrawerToggleHandler} />
+            <div className={classes.Logo}>
+                <Logo></Logo>
+            </div>
+            <nav className={classes.Desktop}>
                 <NavigationItems/>
             </nav>
 
